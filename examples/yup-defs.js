69fd39cd0e0afb90fb02b9ee16671bf44085c288
Tomokatsu Iguchi <tomo14916253@gmail.com>
2022-12-03 12:05:33 +0900

    [fix] example

--- examples/yup-defs.js
+++ examples/yup-defs.js
@@ -6,161 +6,161 @@ import { object, string, number, date, array, bool } from 'yup';
 
 export const LightingSummary = object({
   zones: array().of(object({
-    id: string().optional(),
-    name: string().optional(),
-    deviceId: number().integer().optional(),
-    deviceType: string().optional(),
-    zone: string().optional(),
-  })).optional(),
+    id: string(),
+    name: string(),
+    deviceId: number().integer(),
+    deviceType: string(),
+    zone: string(),
+  })),
   zoneStatus: array().of(object({
-    id: string().optional(),
-    name: string().optional(),
-    lastUpdate: date().optional(),
-    level: number().integer().optional(),
-  }).label('the status of the lighting zone.')).optional(),
+    id: string(),
+    name: string(),
+    lastUpdate: date(),
+    level: number().integer(),
+  }).label('the status of the lighting zone.')),
 }).label('ok');
 
 export const LightingZone = object({
-  id: string().optional(),
-  name: string().optional(),
-  deviceId: number().integer().optional(),
-  deviceType: string().optional(),
-  zone: string().optional(),
+  id: string(),
+  name: string(),
+  deviceId: number().integer(),
+  deviceType: string(),
+  zone: string(),
 });
 
 export const LightingZoneStatus = object({
-  id: string().optional(),
-  name: string().optional(),
-  lastUpdate: date().optional(),
-  level: number().integer().optional(),
+  id: string(),
+  name: string(),
+  lastUpdate: date(),
+  level: number().integer(),
 }).label('the status of the lighting zone.');
 
 export const TemperatureSummary = object({
   zones: array().of(object({
     id: number().integer().label('the unique identifier for the zone').required(),
     name: string().required(),
-    inputPosition: number().integer().optional(),
-    outputPosition: number().integer().optional(),
-    zone: string().optional(),
-  }).label('a single temperature zone')).optional(),
+    inputPosition: number().integer(),
+    outputPosition: number().integer(),
+    zone: string(),
+  }).label('a single temperature zone')),
   zoneStatus: array().of(object({
     id: string().label('the unique identifier for the zone').required(),
-    name: string().label('the name of the zone').optional(),
+    name: string().label('the name of the zone'),
     value: number().label('the temperature in the zone').required(),
-    units: string().label('the temperature units').optional(),
+    units: string().label('the temperature units'),
     timestamp: date().label('the timestamp when the temperature was measured').required(),
-  }).label('status of a single zone')).optional(),
+  }).label('status of a single zone')),
 }).label('ok');
 
 export const TemperatureZone = object({
   id: number().integer().label('the unique identifier for the zone').required(),
   name: string().required(),
-  inputPosition: number().integer().optional(),
-  outputPosition: number().integer().optional(),
-  zone: string().optional(),
+  inputPosition: number().integer(),
+  outputPosition: number().integer(),
+  zone: string(),
 }).label('a single temperature zone');
 
 export const TemperatueZoneStatus = object({
   id: string().label('the unique identifier for the zone').required(),
-  name: string().label('the name of the zone').optional(),
+  name: string().label('the name of the zone'),
   value: number().label('the temperature in the zone').required(),
-  units: string().label('the temperature units').optional(),
+  units: string().label('the temperature units'),
   timestamp: date().label('the timestamp when the temperature was measured').required(),
 }).label('status of a single zone');
 
 export const ApiResponse = object({
-  code: number().integer().optional(),
-  message: string().optional(),
+  code: number().integer(),
+  message: string(),
 });
 
 export const HeaterState = object({
-  id: string().optional(),
-  state: string().optional(),
+  id: string(),
+  state: string(),
 });
 
 export const DeviceState = object({
-  id: string().optional(),
-  name: string().optional(),
-  lastUpdate: date().optional(),
-  level: number().integer().optional(),
+  id: string(),
+  name: string(),
+  lastUpdate: date(),
+  level: number().integer(),
 });
 
 export const ForecastResponse = object({
   city: object({
-    id: number().integer().optional(),
-    name: string().optional(),
-    lat: number().optional(),
-    lon: number().optional(),
-    country: string().optional(),
-  }).optional(),
+    id: number().integer(),
+    name: string(),
+    lat: number(),
+    lon: number(),
+    country: string(),
+  }),
   values: array().of(object({
-    date: date().optional(),
-    pressure: number().optional(),
-    humidity: number().integer().optional(),
-    windSpeed: number().optional(),
-    clouds: number().integer().optional(),
+    date: date(),
+    pressure: number(),
+    humidity: number().integer(),
+    windSpeed: number(),
+    clouds: number().integer(),
     temperature: object({
-      low: number().optional(),
-      high: number().optional(),
-      morning: number().optional(),
-      day: number().optional(),
-      evening: number().optional(),
-      night: number().optional(),
-    }).optional(),
+      low: number(),
+      high: number(),
+      morning: number(),
+      day: number(),
+      evening: number(),
+      night: number(),
+    }),
     weather: object({
-      summary: string().optional(),
-      description: string().optional(),
-      icon: string().optional(),
-    }).optional(),
-  })).optional(),
+      summary: string(),
+      description: string(),
+      icon: string(),
+    }),
+  })),
 });
 
 export const Forecast = object({
-  date: date().optional(),
-  pressure: number().optional(),
-  humidity: number().integer().optional(),
-  windSpeed: number().optional(),
-  clouds: number().integer().optional(),
+  date: date(),
+  pressure: number(),
+  humidity: number().integer(),
+  windSpeed: number(),
+  clouds: number().integer(),
   temperature: object({
-    low: number().optional(),
-    high: number().optional(),
-    morning: number().optional(),
-    day: number().optional(),
-    evening: number().optional(),
-    night: number().optional(),
-  }).optional(),
+    low: number(),
+    high: number(),
+    morning: number(),
+    day: number(),
+    evening: number(),
+    night: number(),
+  }),
   weather: object({
-    summary: string().optional(),
-    description: string().optional(),
-    icon: string().optional(),
-  }).optional(),
+    summary: string(),
+    description: string(),
+    icon: string(),
+  }),
 });
 
 export const City = object({
-  id: number().integer().optional(),
-  name: string().optional(),
-  lat: number().optional(),
-  lon: number().optional(),
-  country: string().optional(),
+  id: number().integer(),
+  name: string(),
+  lat: number(),
+  lon: number(),
+  country: string(),
 });
 
 export const ForecastTemperature = object({
-  low: number().optional(),
-  high: number().optional(),
-  morning: number().optional(),
-  day: number().optional(),
-  evening: number().optional(),
-  night: number().optional(),
+  low: number(),
+  high: number(),
+  morning: number(),
+  day: number(),
+  evening: number(),
+  night: number(),
 });
 
 export const WeatherForecast = object({
-  summary: string().optional(),
-  description: string().optional(),
-  icon: string().optional(),
+  summary: string(),
+  description: string(),
+  icon: string(),
 });
 
 export const DeviceRegistrationInfo = object({
-  uri: string().optional(),
-  id: string().optional(),
+  uri: string(),
+  id: string(),
 });
 
